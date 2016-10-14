9c183de1007e92cebca58cd1954e83e3220ed100
Ryan Macdonald <ryan.macdonald@shopify.com>
2016-10-14 16:56:42 -0400

    more stuff

--- index.js
+++ index.js
@@ -3,33 +3,62 @@
 const join = require('path').join;
 const spawn = require('child_process').spawn;
 const debug = require('debug')('slate-cli');
+const chalk = require('chalk');
 const minimist = require('minimist');
 const themeRoot = require('find-root')(process.cwd());
 
-const pkg = require(join(themeRoot, 'package.json'));
-const commands = minimist(process.argv.slice(2))._;
-
-if ('dependencies' in pkg && 'slate-tools' in pkg.dependencies) {
-  debug('✓ This package.json has required dependency slate-tools');
-} else {
-  throw new Error('package.json missing dependency slate-tools. Try `npm install slate-tools`.');
-}
-
-function newTheme() {
-  console.log('  I wish I could make a new theme...');
-  return;
-}
+const pkg = require(join(__dirname, 'package.json'));
+const themePkg = require(join(themeRoot, 'package.json'));
+const argv = minimist(process.argv.slice(2));
 
 debug(process.argv.slice(2));
+debug(argv);
 
-if (commands.length > 0 && commands[0] === 'new' && commands[1] === 'theme') {
-  console.log('  This may take some time...');
-  console.log('');
-  newTheme();
-} else {
-  debug('Searching slate-tools...');
-
-  spawn('slate-tools', process.argv.slice(2), {
-    stdio: 'inherit'
+function newTheme() {
+  return new Promise((resolve) => {
+    console.log('  I wish I could make a new theme...');
+    resolve();
   });
 }
+
+function checkThemeDeps() {
+  return new Promise((resolve, reject) => {
+    if (('dependencies' in themePkg && 'slate-tools' in themePkg.dependencies) || ('devDependencies' in themePkg && 'slate-tools' in themePkg.devDependencies)) {
+      resolve(`${chalk.green('✓')} package.json has required dependency: slate-tools`);
+    } else {
+      reject(`${chalk.red('✗')} package.json missing dependency slate-tools. Try \`npm install slate-tools\`.`);
+    }
+  });
+}
+
+if (argv.version || argv.v) {
+  console.log(`  slate-cli ${pkg.version}`);
+
+  return checkThemeDeps()
+    .then(() => {
+      console.log(`  slate-tools ${themePkg.version}`);
+      return;
+    })
+    .catch(() => {
+      return;
+    });
+}
+
+if (argv._.length > 0 && argv._[0] === 'new' && argv._[1] === 'theme') {
+  console.log('  This may take some time...');
+  console.log('');
+  return newTheme();
+}
+
+checkThemeDeps()
+  .then((response) => {
+    debug(response);
+    debug('Searching slate-tools...');
+
+    return spawn('slate-tools', process.argv.slice(2), {
+      stdio: 'inherit'
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
