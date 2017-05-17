1cecf0a071eae99d3bade25169b8c71b146e19b6
Chris Berthe <chrisberthe@gmail.com>
2017-05-17 09:16:01 -0400

    Updates based on feedback

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,6 +1,8 @@
 export default function(program) {
   program
     .command('help')
+    .alias('h')
+    .description('Output usage information')
     .action(() => {
       program.help();
     });
